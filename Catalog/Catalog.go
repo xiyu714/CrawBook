91bc8c3672dc1f70fb64766c9b8df97e0e5302eb
xiyu <daixiyu714@outlook.com>
2018-10-30 21:55:03 +0800

    需要解决字符编码的问题

--- Catalog/Catalog.go
+++ Catalog/Catalog.go
@@ -2,13 +2,12 @@ package Catalog
 
 import (
 	"CrawBook2/tool"
-	"fmt"
 	"github.com/PuerkitoBio/goquery"
 )
 
 //不能给默认值
 type Catalog struct {
-	id int
+	zhangs [][2]string
 }
 
 func (c Catalog) getCatalog(url string) {
@@ -20,8 +19,14 @@ func (c Catalog) getCatalog(url string) {
 	doc.Find("#list > dl:nth-child(1)").Each(func(i int, s *goquery.Selection) {
 		s.Children().Each(func(j int, selection *goquery.Selection) {
 			str := selection.Text()
+			value, ok := selection.Attr("href")
+			if !ok {
+				println("no")
+			}
 			str = tool.Utf8(str)
-			fmt.Println(str)
+			println(str)
+			println(value)
+			c.zhangs = append(c.zhangs, [2]string{value, str})
 		})
 	})
 
--- Catalog/Catalog_test.go
+++ Catalog/Catalog_test.go
@@ -1,10 +1,20 @@
 package Catalog
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestCatalog(t *testing.T) {
 	var c Catalog
 	c.getCatalog("http://www.biquge.com.tw/18_18550/")
+}
+
+func TestDarray(t *testing.T) {
+	var as [][2]string
+	//不能使用["hello", "world"]的形式创建数组
+	as = append(as, [2]string{"hello", "world"})
+
+	fmt.Println(as)
+
 }
