a1f487832b94cdc641f51d6b245d672623a78100
xiyu <daixiyu714@outlook.com>
2018-10-31 09:46:36 +0800

    准备重写doc.Find

--- Catalog/Catalog.go
+++ Catalog/Catalog.go
@@ -1,8 +1,10 @@
 package Catalog
 
 import (
-	"CrawBook2/tool"
+	"CrawBook/github.com/axgle/mahonia"
+	"fmt"
 	"github.com/PuerkitoBio/goquery"
+	"net/http"
 )
 
 //不能给默认值
@@ -11,19 +13,36 @@ type Catalog struct {
 }
 
 func (c Catalog) getCatalog(url string) {
-	doc, err := goquery.NewDocument(url)
+	decoder := mahonia.NewDecoder("gbk")
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+
+	rd := decoder.NewReader(resp.Body)
+
+	doc, err := goquery.NewDocumentFromReader(rd)
 	if err != nil {
 		println(err)
 	}
 
+	var str string
+	var value string
+	var ok bool
 	doc.Find("#list > dl:nth-child(1)").Each(func(i int, s *goquery.Selection) {
 		s.Children().Each(func(j int, selection *goquery.Selection) {
-			str := selection.Text()
-			value, ok := selection.Attr("href")
+			//strh, err := selection.Html()
+			//if err != nil {
+			//	fmt.Println(err)
+			//}
+			//-- 测试能否读取内部html
+			str = selection.Text()
+			value, ok = selection.Attr("href")
 			if !ok {
 				println("no")
 			}
-			str = tool.Utf8(str)
 			println(str)
 			println(value)
 			c.zhangs = append(c.zhangs, [2]string{value, str})
