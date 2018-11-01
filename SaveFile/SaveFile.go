7b3977645131dde83e8ea5a302f43e031647cedc
xiyu <daixiyu714@outlook.com>
2018-11-01 16:27:38 +0800

    使用sync.WaitGroup实现了让所有线程同步

--- SaveFile/SaveFile.go
+++ SaveFile/SaveFile.go
@@ -6,12 +6,14 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"sync"
 )
 
 type SaveFile struct {
 }
 
 var abuf []string
+var wg sync.WaitGroup
 
 func (s *SaveFile) save(c Catalog) {
 	//var buf bytes.Buffer
@@ -22,8 +24,8 @@ func (s *SaveFile) save(c Catalog) {
 		fmt.Println(err)
 	}
 	for i, u := range c.Zhangs {
-		addOne(i, u)
-		fmt.Println(u[1])
+		wg.Add(1)
+		go addOne(i, u)
 
 		//buf.WriteString(u[1])
 		//fmt.Println(u[1])
@@ -31,10 +33,13 @@ func (s *SaveFile) save(c Catalog) {
 		//buf.WriteString(tool.GetOneChapter(u[0]))
 		//buf.WriteString("\n")
 	}
+	wg.Wait()
 	str := strings.Join(abuf, "")
-	file.Write([]byte(str))
+	file.Write([]byte(str)) //这里可能有问题
 }
 
 func addOne(i int, u [2]string) {
 	abuf[i] = u[1] + "\n" + tool.GetOneChapter(u[0]) + "\n"
+	fmt.Println(u[1])
+	wg.Done()
 }
