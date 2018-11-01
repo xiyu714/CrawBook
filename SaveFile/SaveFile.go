1b25baf485eba936ddb6b058a97fffc963854cbb
xiyu <daixiyu714@outlook.com>
2018-11-01 13:25:51 +0800

    完成使用变长数组类似的功能

--- SaveFile/SaveFile.go
+++ SaveFile/SaveFile.go
@@ -3,28 +3,32 @@ package SaveFile
 import (
 	. "CrawBook2/Catalog"
 	"CrawBook2/tool"
-	"bytes"
 	"fmt"
 	"os"
+	"strings"
 )
 
 type SaveFile struct {
 }
 
 func (s *SaveFile) save(c Catalog) {
-	var buf bytes.Buffer
+	//var buf bytes.Buffer
+	abuf := make([]string, len(c.Zhangs))
 
 	file, err := os.Create("test.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
-	for _, u := range c.Zhangs {
-		buf.WriteString(u[1])
+	for i, u := range c.Zhangs {
+		abuf[i] = u[1] + "\n" + tool.GetOneChapter(u[0]) + "\n"
 		fmt.Println(u[1])
-		buf.WriteString("\n")
-		buf.WriteString(tool.GetOneChapter(u[0]))
-		buf.WriteString("\n")
-	}
 
-	file.Write(buf.Bytes())
+		//buf.WriteString(u[1])
+		//fmt.Println(u[1])
+		//buf.WriteString("\n")
+		//buf.WriteString(tool.GetOneChapter(u[0]))
+		//buf.WriteString("\n")
+	}
+	str := strings.Join(abuf, "")
+	file.Write([]byte(str))
 }
